f0ee7987e5acd0a7eeeb2044fa4b6a27646705e2
Chris Aumann <me@chr4.org>
2014-09-02 11:05:35 +0200

    Remove extra blank lines

--- recipes/nfs_client.rb
+++ recipes/nfs_client.rb
@@ -17,7 +17,6 @@
 # limitations under the License.
 #
 
-
 package 'nfs-common'
 
 mount '/var/lib/one' do
--- recipes/node.rb
+++ recipes/node.rb
@@ -17,7 +17,6 @@
 # limitations under the License.
 #
 
-
 package 'opennebula-node'
 package 'bridge-utils'
 
