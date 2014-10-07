82857b01f4375803769a98b81d4933a846ac441d
Chris Aumann <me@chr4.org>
2014-10-07 17:04:04 +0200

    Create mariadb_galera_init recipe

---
+++ recipes/mariadb_galera_init.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: opennebula_ng
+# Recipe:: mariadb_galera_init
+#
+# Copyright 2014, Vaamo Finanz AG
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Install MariaDB + Galera, start first cluster node
+include_recipe 'mysqld::mariadb_galera_init'
